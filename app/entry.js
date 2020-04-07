3390986ce76622d99e48e133f650c255a6aad254
Acaruso <anthonycaruso102@gmail.com>
2020-04-06 21:13:39 -0400

    react working

---
+++ app/entry.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+    <div>react1111!!!</div>,
+    document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path")
+const webpack = require("webpack")
+
+module.exports = {
+    entry: "./app/entry.js",
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: "babel-loader",
+                options: { presets: ["@babel/env", "@babel/preset-react"] }
+            }
+        ]
+    },
+    resolve: {
+        extensions: ["*", ".js", ".jsx"]
+    },
+    output: {
+        path: path.resolve(__dirname, "dist/"),
+        publicPath: "/dist/",
+        filename: "bundle.js"
+    },
+    plugins: []
+};
