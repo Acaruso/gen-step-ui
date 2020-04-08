2c45d1d8e0d502dbe96533161cdcfd4933f96e3c
Acaruso <anthonycaruso102@gmail.com>
2020-04-08 08:54:45 -0400

    more css

--- app/App.js
+++ app/App.js
@@ -1,22 +1,46 @@
-import React from "react";
+import React, { useState } from 'react';
 import constants from "./constants";
 import { writeSong } from "./midi";
 
 export default function App() {
-  const handleClick = () => {
-    writeSong('test1.mid', constants.testSong);
+  const defaultNumSteps = 4;
+  let defaultSquareData = getEmptySquareData(defaultNumSteps);
+
+  const [numSteps, setNumSteps] = useState(defaultNumSteps);
+  const [squareData, setSquareData] = useState(defaultSquareData);
+
+  const onSquareClick = (index) => {
+    let newSquareData = getEmptySquareData(numSteps);
+    newSquareData[index] = true;
+    setSquareData(newSquareData);
   };
+
+  const squares = squareData.map((elt, i) => {
+    return <Square selected={elt} onSquareClick={onSquareClick} index={i} />;
+  });
 
   return (
     <>
-      <div onClick={handleClick}>Open File</div>
-      {/* <div className="container" style={{display: "flex"}}> */}
       <div className="container">
-        <span className="item"></span>
-        <span className="item"></span>
-        <span className="item"></span>
-        <span className="item"></span>
+        {squares}
       </div>
     </>
   );
+}
+
+function Square({selected, onSquareClick, index}) {
+  return (
+    <span 
+      className={selected ? "item selected-item" : "item"}
+      onClick={() => onSquareClick(index)}
+    />
+  );
+}
+
+function getEmptySquareData(numSteps) {
+  let emptySquareData = [];
+  for (let i = 0; i < numSteps; i++) {
+    emptySquareData.push(false);
+  }
+  return emptySquareData;
 }
