19cd9a093f8ef22ed2ff494f1f1c9d9c02f3204c
Acaruso <anthonycaruso102@gmail.com>
2020-04-12 21:15:48 -0400

    cleanup

--- app/redux/reducers/rootReducer.js
+++ app/redux/reducers/rootReducer.js
@@ -1,9 +1,7 @@
 import { combineReducers } from "redux";
-import testReducer from "./testReducer";
 import tracksReducer from "../slices/trackSlice";
 
 const rootReducer = combineReducers({
-  testVal: testReducer,
   tracks: tracksReducer
 });
 
--- app/redux/store.js
+++ app/redux/store.js
@@ -1,8 +1,5 @@
-// import { createStore } from "redux";
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./reducers/rootReducer"
-
-// const store = createStore(rootReducer);
 
 const store = configureStore({
   reducer: rootReducer,
