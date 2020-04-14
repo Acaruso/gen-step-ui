1c67a6b38e4c042da990fc0c8e7dcb1d868d0184
Acaruso <anthonycaruso102@gmail.com>
2020-04-14 19:46:10 -0400

    add loop

--- app/Song.js
+++ app/Song.js
@@ -1,13 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Track from "./Track";
 import EventEditor from "./EventEditor";
 import { addTrack, deleteTrackByName } from './redux/slices/trackSlice';
 import { connect } from "react-redux";
+import * as Tone from "tone";
 
 
 function Song({tracks, addTrack, deleteTrackByName}) {
   const [trackNameValue, setTrackNameValue] = useState('');
   const [deleteTrackNameValue, setDeleteTrackNameValue] = useState('');
+  const [loop, setLoop] = useState({});
+
+  useEffect(() => {
+    Tone.Transport.bpm.value = 120;
+    Tone.Transport.start();
+
+    const _loop = new Tone.Loop(time => {
+      console.log('loop')
+      console.log(time)
+    }, "16n");
+    setLoop(_loop);
+  }, []);
 
   const trackElts = tracks.ids.map((id) => {
     const track = tracks.items[id];
@@ -32,23 +45,35 @@ function Song({tracks, addTrack, deleteTrackByName}) {
     setTrackNameValue(e.target.value);
   }
 
+  function onClickPlay() {
+    loop.start(0);
+  }
+
+  function onClickStop() {
+    loop.stop();
+  }
+
   return (
-    <div className="grid">
-      <div>
+    <>
+      <div className="grid">
         <div>
-          <button>Load Song</button>
-          <button>Save Song</button>
+          <div>
+            <button>Load Song</button>
+            <button>Save Song</button>
+          </div>
+          <div>
+            <button onClick={onAddTrack}>Add Track</button>
+            <input type="text" value={trackNameValue} onChange={onChangeTrackName} />
+            <button onClick={onDeleteTrack}>Delete Track</button>
+            <input type="text" value={deleteTrackNameValue} onChange={onChangeDeleteTrackName} />
+          </div>
+          {trackElts}
         </div>
-        <div>
-          <button onClick={onAddTrack}>Add Track</button>
-          <input type="text" value={trackNameValue} onChange={onChangeTrackName} />
-          <button onClick={onDeleteTrack}>Delete Track</button>
-          <input type="text" value={deleteTrackNameValue} onChange={onChangeDeleteTrackName} />
-        </div>
-        {trackElts}
+        <EventEditor />
       </div>
-      <EventEditor />
-    </div>
+      <button onClick={onClickPlay}>Play</button>
+      <button onClick={onClickStop}>Stop</button>
+    </>
   );
 }
 
